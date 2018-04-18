6156b6b547a8e4685c7cf02a7797b48f9cdbacb7
John Hörnvall <trolledwoods@gmail.com>
2018-04-18 20:08:35 +0200

    Stopped exceding vertex limit + added temporary water

--- RTSgameInUnity/Assets/Scripts/GameController.cs
+++ RTSgameInUnity/Assets/Scripts/GameController.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 using Assets.Scripts;
 using Assets.Scripts.WorldGenerators;
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshCollider))]
 public class GameController : MonoBehaviour {
-    
+
+    public Material world_material;
+
     void Start()
     {
-        World w = new World(300, 217,
+        World w = new World(2000, 2000,
             new Classic(0.03f, 18f, 0.01f, 6));
         WorldRenderer renderer = new WorldRenderer(
-            w,
+            this.gameObject, w,
             new Color32[]
             {
                 new Color32(120, 72, 0, 255),
                 new Color32(1, 142, 14, 255),
                 new Color32(194, 178, 128, 255)
-            },
-            GetComponent<MeshFilter>().mesh,
-            GetComponent<MeshCollider>());
+            }, world_material);
     }
 
 }
--- RTSgameInUnity/Assets/Scripts/WorldRenderer.cs
+++ RTSgameInUnity/Assets/Scripts/WorldRenderer.cs
@@ -8,86 +8,192 @@ namespace Assets.Scripts
 {
     public class WorldRenderer
     {
-        World game_world;
-
-        Mesh world_mesh;
-        MeshCollider world_collider;
-
-        Color32[] vertex_colors;
-
-        Vector3[] vertices;
-        Color32[] colors;
-        int[] triangles;
+        static int SectionSize = 200;
         
-        public WorldRenderer(World game_world, Color32[] vertex_colors,
-            Mesh world_mesh, MeshCollider world_collider = null)
-        {
-            this.game_world = game_world;
+        World game_world;
+        
+        Color32[] vertex_colors;
+        Material material;
 
-            this.world_mesh = world_mesh;
-            this.world_collider = world_collider;
+        GameObject mesh_parent;
+        MeshController[] controllers;
+
+        public WorldRenderer(GameObject mesh_parent, World game_world, Color32[] vertex_colors, Material material)
+        {
+            this.mesh_parent = mesh_parent;
+
             this.vertex_colors = vertex_colors;
 
-            vertices = new Vector3[game_world.Vertex_Count];
-            colors = new Color32[game_world.Vertex_Count];
-            triangles = new int[game_world.Tile_Count * 2 * 3];
+            this.game_world = game_world;
+            this.material = material;
 
             CreateMesh();
         }
 
         public void CreateMesh()
         {
-            int vert_count = game_world.Vertex_Count;
-            int vert_width = game_world.Vertex_Width;
-            int vert_height = game_world.Vertex_Height;
-
-            int tile_width = game_world.Tile_Width;
-            int tile_height = game_world.Tile_Height;
-
-            int vert_index = 0;
-            for (int j = 0; j < vert_height; j++)
+            // Clear all objects in the containers array
+            if(controllers != null)
             {
-                for (int i = 0; i < vert_width; i++)
+                for(int i = controllers.Length - 1; i >= 0; i--)
                 {
-                    vertices[vert_index] = new Vector3(i, game_world.Vertices[vert_index].Height, j);
-                    colors[vert_index] = vertex_colors[(int)game_world.Vertices[vert_index].Type];
-                    vert_index++;
+                    GameObject.Destroy(controllers[i].Container);
                 }
             }
 
-            vert_index = 0;
-            int tri_index = 0;
-            for(int j = 0; j < tile_height; j++)
-            {
-                for(int i = 0; i < tile_width; i++)
-                {
-                    // Triangle 1
-                    triangles[tri_index + 0] = vert_index + vert_width;
-                    triangles[tri_index + 1] = vert_index + 1;
-                    triangles[tri_index + 2] = vert_index + 0;
+            int n_sections_width = Int_Ceil(game_world.Tile_Width, SectionSize);
+            int n_sections_height = Int_Ceil(game_world.Tile_Height, SectionSize);
+            int current_section = 0;
 
-                    // Triangle 2
-                    triangles[tri_index + 3] = vert_index + vert_width;
-                    triangles[tri_index + 4] = vert_index + vert_width + 1;
-                    triangles[tri_index + 5] = vert_index + 1;
+            int left = 0;
+            int right;
+            int bottom;
+            int top;
+
+            controllers = new MeshController[n_sections_width * n_sections_height];
+   
+            for (int i = 0; i < n_sections_width; i++)
+            {
+                bottom = 0;
+
+                for(int j = 0; j < n_sections_height; j++)
+                {
+                    right = left + SectionSize + 1;
+                    top = bottom + SectionSize + 1;
+
+                    right = right >= game_world.Vertex_Width ? 
+                        game_world.Vertex_Width - 1 : right;
+                    top = top >= game_world.Vertex_Height ? 
+                        game_world.Vertex_Height - 1 : top;
+
+                    controllers[current_section] = new MeshController(left, right, 
+                        bottom, top, 
+                        game_world,
+                        vertex_colors, material);
+                    controllers[current_section].Container.transform.parent = mesh_parent.transform;
                     
-                    vert_index++;
-                    tri_index += 6;
+                    bottom += SectionSize;
+                    current_section++;
                 }
-                vert_index++;
+                left += SectionSize;
+            }
+        }
+
+        public int Int_Ceil(int a, int b)
+        {
+            return (a / b) + (a % b == 0 ? 0 : 1);
+        }
+
+        class MeshController
+        {
+            World game_world;
+            
+            GameObject container;
+            public GameObject Container { get { return container; } }
+            
+            public Color32[] Vertex_Colors;
+
+            Mesh controlling;
+            MeshCollider collider;
+
+            Vector3[] vertices;
+            Color32[] colors;
+            int[] triangles;
+
+            int vert_left, vert_right, vert_top, vert_bottom;
+            int vert_width, vert_height, tile_width, tile_height;
+
+            public MeshController(int left, int right, int bottom, int top, World game_world, Color32[] vertex_colors, Material material)
+            {
+                container = new GameObject("World Section", 
+                    typeof(MeshRenderer), 
+                    typeof(MeshFilter),
+                    typeof(MeshCollider));
+                container.transform.position = new Vector3(left, 0, bottom);
+
+                container.layer = 8;
+                controlling = container.GetComponent<MeshFilter>().mesh;
+                container.GetComponent<MeshRenderer>().material = material;
+                collider = container.GetComponent<MeshCollider>();
+
+                this.game_world = game_world;
+
+                this.vert_left = left;
+                this.vert_right = right;
+                this.vert_top = top;
+                this.vert_bottom = bottom;
+
+                vert_width = right - left;
+                vert_height = top - bottom;
+                tile_width = vert_width - 1;
+                tile_height = vert_height - 1;
+                
+                Vertex_Colors = vertex_colors;
+
+                // Create arrays
+                colors = new Color32[vert_width * vert_height];
+                vertices = new Vector3[vert_width * vert_height];
+                Debug.Log(vert_width + " " + vert_height);
+                triangles = new int[(vert_width - 1) * (vert_height - 1) * 6];
+
+                CreateMesh();
             }
 
-            // Set the values of the mesh
-            world_mesh.Clear();
-            world_mesh.vertices = vertices;
-            world_mesh.colors32 = colors;
-            world_mesh.triangles = triangles;
-            world_mesh.RecalculateNormals();
-            world_mesh.RecalculateBounds();
-            
-            // Update the mesh collider
-            if (world_collider != null)
-                world_collider.sharedMesh = world_mesh;
+            public void CreateMesh()
+            {
+                // Instantiate some useful variables
+                int local_vert_i;
+                int global_vert_i;
+                int tri_i;
+                World.Vertex read_vert;
+
+                // Create vertices
+                for (int j = 0; j < vert_height; j++)
+                {
+                    for (int i = 0; i < vert_width; i++)
+                    {
+                        local_vert_i = i + j * vert_width;
+                        global_vert_i = (i + vert_left) + (j + vert_bottom) * game_world.Vertex_Width;
+
+                        read_vert = game_world.Vertices[global_vert_i];
+                        vertices[local_vert_i] = new Vector3(i, read_vert.Height, j);
+                        colors[local_vert_i] = Vertex_Colors[(int)read_vert.Type];
+                    }
+                }
+
+                // Create triangles
+                tri_i = 0;
+                for (int j = 0; j < tile_height; j++)
+                {
+                    for (int i = 0; i < tile_width; i++)
+                    {
+                        local_vert_i = i + j * vert_width;
+
+                        triangles[tri_i + 0] = local_vert_i + 0;
+                        triangles[tri_i + 1] = local_vert_i + vert_width + 1;
+                        triangles[tri_i + 2] = local_vert_i + 1;
+
+                        triangles[tri_i + 3] = local_vert_i + 0;
+                        triangles[tri_i + 4] = local_vert_i + vert_width;
+                        triangles[tri_i + 5] = local_vert_i + vert_width + 1;
+
+                        tri_i += 6;
+                    }
+                }
+
+                // Set the values of the mesh
+                controlling.Clear();
+                controlling.vertices = vertices;
+                controlling.colors32 = colors;
+                controlling.triangles = triangles;
+                controlling.RecalculateNormals();
+                controlling.RecalculateBounds();
+
+                // Update the mesh collider
+                if (collider != null)
+                    collider.sharedMesh = controlling;
+            }
         }
+
     }
 }
